a8008891278d56670fee07a3e71ae3e646638226
Ankush Chauhan <ankush.26.11@gmail.com>
2016-09-13 14:16:06 -0400

    Add files via upload

---
+++ Readme.txt
@@ -0,0 +1,11 @@
+
+Author  : Ankush Chauhan
+E-Mail  : achauhan4@student.gsu.edu
+
+Steps to test the Work.
+Step 1  : Required Android Emulator / Phone must have Android OS KitKat or newer.
+Step 2  : Download and Install "app.apk" on Emulator / Phone.
+Step 3  : Tap on "Ankush Chauhan" to edit and enter your own name.
+Step 4  : Tap on "Hello" or "Goodbye" to view messages
+
+View screenshots in the archive.
--- app/src/main/java/tech/ankush/helloworldandroid/HelloWorldActivity.java
+++ app/src/main/java/tech/ankush/helloworldandroid/HelloWorldActivity.java
@@ -1,7 +1,7 @@
 package tech.ankush.helloworldandroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,7 +9,9 @@ import android.widget.TextView;
 
 public class HelloWorldActivity extends AppCompatActivity {
     private EditText enteredName;
-    private TextView printedName;
+    private TextView printHello;
+    private TextView printBye;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,18 +20,18 @@ public class HelloWorldActivity extends AppCompatActivity {
     public void sayHello(View v){
         Button helloButton = (Button) v;
         enteredName = (EditText) findViewById(R.id.editText);
-        printedName = (TextView) findViewById(R.id.printMsg);
-        printedName.setText("Hello " + enteredName.getText());
-        printedName.setVisibility(View.VISIBLE);
+        printHello = (TextView) findViewById(R.id.printHello);
+        printHello.setText("Hello " + enteredName.getText());
+        printHello.setVisibility(View.VISIBLE);
 
     }
 
     public void sayGoodbye(View v){
         Button goodbyeButton = (Button) v;
         enteredName = (EditText) findViewById(R.id.editText);
-        printedName = (TextView) findViewById(R.id.printMsg);
-        printedName.setText("Goodbye " + enteredName.getText());
-        printedName.setVisibility(View.VISIBLE);
+        printBye = (TextView) findViewById(R.id.printBye);
+        printBye.setText("Goodbye " + enteredName.getText());
+        printBye.setVisibility(View.VISIBLE);
 
     }
 
